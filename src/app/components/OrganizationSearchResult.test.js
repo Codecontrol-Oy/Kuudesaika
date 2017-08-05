2675e45c8ae76f4159d0816c1351fe3664fd8c7a
Toni Iltanen <toni.iltanen@codecontrol.fi>
2017-08-06 01:00:20 +0300

    test for organizationtestresults

---
+++ src/app/components/OrganizationSearchResult.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import OrganizationSearchResult from './OrganizationSearchResult';
+import mock from '../../../mock/Organization.json';
+import Article from 'grommet/components/Article';
+
+describe('<OrganizationSearchResults />', () => {
+
+    it('Should match snapshot', () => {
+        const organizationSearchResult = mount(
+            <OrganizationSearchResult organization={mock} onClick={() => {}}   />
+        );
+        expect(organizationSearchResult).toMatchSnapshot();
+    });
+
+    it('Should render components', () => {
+         const organizationSearchResult = mount(
+            <OrganizationSearchResult organization={mock} onClick={() => {}}   />
+        );
+        expect(organizationSearchResult.find('ListItem').length).toEqual(1);
+        expect(organizationSearchResult.find('LatestEvent').length).toEqual(1);
+    });
+});
